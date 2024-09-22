37d711618043078b27d9778882290b84fba3481e
riezoldyck <faire_un_beau_reve@hotmail.com>
2024-09-23 00:18:32 +0900

    クエリビルダを使ってデータを取得しよう

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,22 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>静的なページを表示しよう</title>
+  <title>クエリビルダを使ってデータを取得しよう</title>
 </head>
 
 <body>
-  <h1>投稿一覧</h>
+  <table>
+    <tr>
+      <th>タイトル</th>
+      <th>本文</th>
+    </tr>
+    @foreach($posts as $post)
+      <tr>
+        <td>{{ $post->title }}</td>
+        <td>{{ $post->content }}</td>
+      </tr>
+    @endforeach
+  </table>
 </body>
 
 </html>
